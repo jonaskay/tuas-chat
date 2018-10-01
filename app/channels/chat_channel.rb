fa9d02426b42e7af5cf3a37750100ebe6b9f2e04
Jonas Kay <jonas-kay@outlook.com>
2018-10-02 00:33:31 +0300

    Add ChatChannel

---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,9 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from 'chat_channel'
+  end
+
+  def create(opts)
+    Message.create(body: opts.fetch('body'))
+  end
+end
