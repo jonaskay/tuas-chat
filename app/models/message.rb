ff400b79a60042cd696eb4917780384b08659775
Jonas Kay <jonas-kay@outlook.com>
2018-10-02 00:33:52 +0300

    Broadcast created message to chat_channel

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
 class Message < ApplicationRecord
   validates :body, presence: true
 
+  after_create do
+    ActionCable.server.broadcast(
+      'chat_channel',
+      id: self.id,
+      created_at: self.created_at.strftime('%H:%M'),
+      body: self.body
+    )
+  end
 end
